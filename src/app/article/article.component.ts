67b143e42f884c61664494f8c5a351c983dba755
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-05-19 11:01:41 +0200

    Property binding

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -10,6 +10,8 @@ export class ArticleComponent implements OnInit {
   titreArticle: string = "Titre de l'article!";
   prixArticle: number = 12;
   ctrlArticle: boolean = true;
+  textAltImg: string = "Titre alternative de l'image";
+  urlImg: string = "https://via.placeholder.com/400x250";
 
   constructor() { }
 
@@ -21,9 +23,13 @@ export class ArticleComponent implements OnInit {
     if (this.ctrlArticle === true) {
       this.titreArticle = "Titre de l'article!";
       this.ctrlArticle = false;
+      this.textAltImg = "Titre alternative de l'image L"
+      this.urlImg = "https://via.placeholder.com/600x375";
     } else {
       this.titreArticle = "---";
       this.ctrlArticle = true;
+      this.textAltImg = "Titre alternative de l'image"
+      this.urlImg = "https://via.placeholder.com/400x250";
 
     }
   }
