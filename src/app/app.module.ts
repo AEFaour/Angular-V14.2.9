124543aeac5464754555b5d77ca9d4b4361e188c
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-05-22 16:27:50 +0200

    reactives forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DataService } from './data.service';
 import { ListeComponent } from './liste/liste.component';
 import { DetailsComponent } from './details/details.component';
@@ -23,7 +23,8 @@ import { AjouterComponent } from './ajouter/ajouter.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
