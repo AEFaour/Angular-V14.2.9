d198323f5946edc920206af9413a896093b2af1c
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-05-22 15:54:09 +0200

    set up ngForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { FormsModule } from '@angular/forms';
 import { DataService } from './data.service';
 import { ListeComponent } from './liste/liste.component';
 import { DetailsComponent } from './details/details.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AjouterComponent } from './ajouter/ajouter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleComponent,
     ListeComponent,
-    DetailsComponent
+    DetailsComponent,
+    PageNotFoundComponent,
+    AjouterComponent
   ],
   imports: [
     BrowserModule,
